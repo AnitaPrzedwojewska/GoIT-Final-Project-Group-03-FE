03e0ee6bbc4c6353d9ad153d87d5a4cb8ead68fb
AnitaPrzedwojewska <nisiek@wp.pl>
2024-07-31 16:25:41 +0200

    deploy settings for netlify

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename='/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
