22ce4c09b8e1229b6af30be7b6b264adb9b2adc1
AnitaPrzedwojewska <nisiek@wp.pl>
2024-08-01 14:48:37 +0200

    FollowUs component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,20 @@
-// import css from "./Footer.module.css";
-import PropTypes from "prop-types";
+import css from "./Footer.module.css";
 
 import Nav from '../Nav/Nav';
 import FollowUs from '../FollowUs/FollowUs'
 import SubscribeForm from '../SubscribeForm/SubscribeForm'
 
-const Footer = ({ children }) => {
+const Footer = () => {
   return (
     <>
       <div>Footer</div>
       <Nav></Nav>
-      <FollowUs></FollowUs>
+      <div className={css.followUs}>
+        <FollowUs />
+      </div>
       <SubscribeForm></SubscribeForm>
-      <div>{children}</div>
     </>
   );
-};
-
-Footer.propTypes = {
-  children: PropTypes.node,
 };
 
 export default Footer;
