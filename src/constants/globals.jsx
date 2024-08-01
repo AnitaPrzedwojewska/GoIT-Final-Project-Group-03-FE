d4230e30e8a20c3546b3177c7ab4bb845cb9dc04
Wiola-D <wioletta.d.work@gmail.com>
2024-08-01 02:41:43 +0200

    Add constants classic-buttons and module.css

---
+++ src/constants/globals.jsx
@@ -0,0 +1,35 @@
+import css from "./global.module.css";
+
+// export const CLASSIC_BLACK_BUTTON = ({ label, onClick }) => (
+//   <button onClick={onClick}>{label}</button>
+// );
+
+export const CLASSIC_SUBSCRIBE_BUTTON = ({ label, onClick }) => (
+  <button className={`${css.subscribe_classic_button}`} onClick={onClick}>
+    {label}
+  </button>
+);
+
+export const CLASSIC_REGISTER_BUTTON = ({ label, onClick }) => (
+  <button className={`${css.register_classic_button}`} onClick={onClick}>
+    {label}
+  </button>
+);
+
+export const CLASSIC_LOGOUT_BUTTON = ({ onClick }) => (
+  <button
+    className={`${css.small_classic_button} ${css["small_classic_button_logout"]}`}
+    onClick={onClick}
+  >
+    Log out
+  </button>
+);
+
+export const CLASSIC_CANCEL_BUTTON = ({ onClick }) => (
+  <button
+    className={`${css.small_classic_button} ${css["small_classic_button_cancel"]}`}
+    onClick={onClick}
+  >
+    Cancel
+  </button>
+);
