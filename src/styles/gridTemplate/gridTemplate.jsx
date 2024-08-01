53415881696c631afacfde7279e7cc279d15262e
AnitaPrzedwojewska <nisiek@wp.pl>
2024-08-02 01:56:00 +0200

    fixed common styles

--- src/styles/gridTemplate/gridTemplate.jsx
+++ src/styles/gridTemplate/gridTemplate.jsx
@@ -1,31 +1,44 @@
-import css from "./gridTemplate.module.css";
+import css from "gridTemplate.module.css";
 
 <>
     <section className={css.container} id="componentName">
+        
         <div className={css.component}>
             
-            <h1> Element 1 </h1>
+            <h1> Lorem ipsum </h1>
+
             <div className={css.contentContainer}>
                 <div className={css.box}>
-                    <div className={css.myClassName}>
-                        <img/>
-                        <h2></h2>
-                        <p></p>
+                    <div className={css.content}>
+                        <img src="" alt="" className={css.myClassName}/>
+                        <h2> Element 1 </h2>
+                        <p> Lorem </p>
+                        <p> Hello world </p>
                     </div>
                 </div>
-            </div>
-
-            <h2> Element 2 </h2>
+            </div>         
+            
             <div className={css.contentContainer}>
                 <div className={css.box}>
-                    <div className={css.myClassName}>
-                        <img/>
-                        <h2></h2>
-                        <p></p>
+                    <div className={css.content}>
+                        <img src="" alt="" className={css.myClassName}/>
+                        <h2> Element 2 </h2>
+                        <p> Lorem </p>
+                        <p> Hello world </p>
                     </div>
                 </div>
-            </div>
+            </div>    
 
+            <div className={css.contentContainer}>
+                <div className={css.box}>
+                    <div className={css.content}>
+                        <img src="" alt="" className={css.myClassName}/>
+                        <h2> Element 3 </h2>
+                        <p> Lorem </p>
+                        <p> Hello world </p>
+                    </div>
+                </div>
+            </div>    
         </div>
     </section>
 </>
