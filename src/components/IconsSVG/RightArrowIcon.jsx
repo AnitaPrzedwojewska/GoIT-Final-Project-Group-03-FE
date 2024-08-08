76b60cf8affd02f2673aa68810109367a0d9d725
Wiola-D <wioletta.d.work@gmail.com>
2024-08-09 01:07:27 +0200

    Add ChooseYourBreakfast

--- src/components/IconsSVG/RightArrowIcon.jsx
+++ src/components/IconsSVG/RightArrowIcon.jsx
@@ -1,16 +1,18 @@
-const RightArrowIcon = (props) => {
+import PropTypes from "prop-types";
+
+const RightArrowIcon = ({ strokeColor = "#FAFAFA", ...props }) => {
   return (
     <svg width="1em" height="1em" fill="none" viewBox="0 0 20 21" {...props}>
       <path
         d="M2.75 9.5H12.75"
-        stroke="#FAFAFA"
+        stroke={strokeColor}
         strokeWidth="1.3"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M9 4.25L14.25 9.5L9 14.75"
-        stroke="#FAFAFA"
+        stroke={strokeColor}
         strokeWidth="1.3"
         strokeLinecap="round"
         strokeLinejoin="round"
@@ -19,4 +21,7 @@ const RightArrowIcon = (props) => {
   );
 };
 
+RightArrowIcon.propTypes = {
+  strokeColor: PropTypes.string,
+};
 export default RightArrowIcon;
