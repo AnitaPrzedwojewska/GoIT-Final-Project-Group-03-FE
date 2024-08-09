2b1611ccf181d9b50db70b6428e3865cd72ffac0
AnitaPrzedwojewska <nisiek@wp.pl>
2024-08-09 02:48:31 +0200

    categories in redux

--- src/redux/recipes/recipes.selectors.js
+++ src/redux/recipes/recipes.selectors.js
@@ -1,5 +1,5 @@
 export const getCategory = (state) => state.recipes.category;
 
-// export const getRecipes = (state) => state.recipes.recipes;
+export const getCategories = (state) => state.recipes.categories
 
-// export const getCategories = (state) => state.recipes.categories
+// export const getRecipes = (state) => state.recipes.recipes;
