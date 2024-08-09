f69a1636fa3695b5c6b63d90462ff998e962fc7d
Marta Tabor <leeloothefifth@googlemail.com>
2024-08-09 19:56:14 +0100

    navigate to recipe id update in route

--- src/components/MyRecipesList/MyRecipesList.jsx
+++ src/components/MyRecipesList/MyRecipesList.jsx
@@ -38,7 +38,7 @@ const MyRecipesList = () => {
   }, [currentPage]);
 
   const handleSeeRecipe = (id) => {
-    navigate(`/recipe/${id}`);
+    navigate(`/recipes/${id}`);
   };
 
   const handleRemoveRecipe = async (id) => {
