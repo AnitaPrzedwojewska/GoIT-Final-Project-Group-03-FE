63efd01261f22f488ba89108cf17cfd406506cb1
AnitaPrzedwojewska <nisiek@wp.pl>
2024-08-10 00:26:50 +0200

    subscribtion

--- src/components/SubscribeForm/SubscribeForm.jsx
+++ src/components/SubscribeForm/SubscribeForm.jsx
@@ -1,13 +1,39 @@
-import css from "./SubscribeForm.module.css";
+// npm packages
+import Notiflix from "notiflix";
 
+// functions
+import useUser from '../../hooks/useUser';
+import subscribeNewsletter from '../../api/others/subscribeNewsletter';
+
+// components
 import SubscribeFormTitle from './SubscribeFormTitle';
 import EmailIcon from "../IconsSVG/EmailIcon";
 
+// styles
+import css from "./SubscribeForm.module.css";
+
 const SubscribeForm = () => {
+
+  const {email} = useUser();
+  console.log('SubscribeForm - email: ', email);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    try {
+      subscribeNewsletter(email);
+      Notiflix.Notify.success("You have subscribed to the newsletter.");
+    } catch (error) {
+      console.log(error)
+      Notiflix.Notify.failure("Something is wrong. Failed subscription.");
+    }
+    form.reset();
+  }
+
   return (
     <div className={css.subscribeForm}>
       <SubscribeFormTitle />
-      <form className={css.form}>
+      <form className={css.form} onSubmit={handleSubmit}>
         <div>
           <div className={css.inputBox}>
             <div className={css.icon}>
@@ -17,11 +43,14 @@ const SubscribeForm = () => {
               className={css.input}
               id='mainInput'
               type='text'
-              placeholder='Enter your email address'
+              placeholder={email}
+              disabled='true'
             />
           </div>
         </div>
-        <button className={css.subscribeBtn}>Subscribe</button>
+        <button type='submit' className={css.subscribeBtn}>
+          Subscribe
+        </button>
       </form>
     </div>
   );
