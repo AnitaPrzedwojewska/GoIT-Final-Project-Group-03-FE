0b7be435bb0442d9bbeea64b05b265128b5d21b6
MagdalenaBernat <magdalena.bernat@onet.pl>
2024-08-10 15:48:09 +0200

    My Recipes

--- src/components/MyRecipesItem/MyRecipesItem.jsx
+++ src/components/MyRecipesItem/MyRecipesItem.jsx
@@ -1,18 +1,43 @@
-// import css from "./MyRecipesItem.module.css";
 import PropTypes from "prop-types";
+import css from "./MyRecipesItem.module.css";
+import ButonFancy from "../ButonFancy/ButonFancy";
+import ButonClassic from "../ButonClassic/ButonClassic";
 
 const MyRecipesItem = ({ recipe, onSeeRecipe, onRemoveRecipe }) => {
   const { _id, preview, title, description, time } = recipe;
 
   return (
     <>
-      <div>
-        <img src={preview} alt={title} />
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <p>Cooking time: {time}</p>
-        <button onClick={() => onSeeRecipe(_id)}>See Recipe</button>
-        <button onClick={() => onRemoveRecipe(_id)}>Remove Recipe</button>
+      <div className={css.recipe}>
+        <div>
+          <img className={css.image} src={preview} alt={title} />
+        </div>
+        <div className={css.recipeContainer}>
+          <div className={css.titleContainer}>
+            <h2 className={css.title}>{title}</h2>
+            <button
+              onClick={() => onRemoveRecipe(_id)}
+              className={css.removeButton}>
+              {/* <BinIcon
+                width={"24px"}
+                height={"24px"}
+                className={css.binIcon}
+              /> */}
+            </button>
+
+          </div>
+          <p className={css.description}>{description}</p>
+          <div className={css.timeContainer}>
+            <p>{time}</p>
+            <ButonFancy
+              height="59px"
+              className={css.seeRecipeButton}
+              onClick={() => onSeeRecipe(_id)}
+            >
+              See Recipe
+            </ButonFancy>
+          </div>
+        </div>
       </div>
     </>
   );
