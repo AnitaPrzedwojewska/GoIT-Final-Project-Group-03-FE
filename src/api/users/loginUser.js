86b8114b8998c7601e1d03cf5df8e153229954c0
AnitaPrzedwojewska <nisiek@wp.pl>
2024-08-11 13:18:09 +0200

    test bad login user

--- src/api/users/loginUser.js
+++ src/api/users/loginUser.js
@@ -2,12 +2,14 @@ import axios from "axios";
 
 import endpoints from "../../constants/apiEndpoints.js";
 
+
 const loginUser = async (userData) => {
 
   const url = endpoints.USER_LOGIN;
 
   try {
-    const response = await axios.post(url, userData);
+    let response = await axios.post(url, userData);
+    console.log('response: ', response.data);
     return response.data;
   } catch (error) {
     console.log(error);
