f25d4a960eb3870fb4a2f57f26fa7cef46eea050
Wiola-D <wioletta.d.work@gmail.com>
2024-08-12 16:45:58 +0200

    Change alert to notiflix

--- src/api/users/registerUser.js
+++ src/api/users/registerUser.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Notiflix from "notiflix";
 import endpoints from "../../constants/apiEndpoints.js";
 
 const registerUser = async (userData) => {
@@ -6,13 +7,19 @@ const registerUser = async (userData) => {
   console.log(`Sending request to URL: ${url}`);
   try {
     const response = await axios.post(url, userData);
-    console.log(`User ${userData.name} registered successfully.`);
+    Notiflix.Notify.success(`User ${userData.name} registered successfully.`);
 
     if (response.status === 200) {
       return true;
     }
   } catch (error) {
-    console.log("Error:", error);
+    console.error("Error:", error);
+
+    if (error.response && error.response.status === 409) {
+      Notiflix.Notify.failure("User already exists with this email.");
+    } else {
+      Notiflix.Notify.failure("An error occurred. Please try again later.");
+    }
 
     return null;
   }
